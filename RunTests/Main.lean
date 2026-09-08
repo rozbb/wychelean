24fc3b83cb1ad9dd658350129c7e8dd0c5e1f7bd
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2026-09-08 23:24:50 +0200

    test: remove test-helper regression suites

--- RunTests/Main.lean
+++ RunTests/Main.lean
@@ -1,5 +1,4 @@
 import RunTests.Basic
-import RunTests.Tests
 import Wychelean.DH.X25519.Tests
 import Wychelean.Hashes.SHA256.Tests
 import Wychelean.Utils.Tests
@@ -16,8 +15,7 @@ namespace RunTests
 open Wychelean
 
 def suites: List Suite :=
-  [ Tests.suites,
-    Utils.Tests.suites,
+  [ Utils.Tests.suites,
     X25519.Tests.suites,
     Hashes.SHA256.Tests.suites ].flatten
 
